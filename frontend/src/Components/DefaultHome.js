fc5624ce7348c49ab67fcf7029d9d0fd8fd69d47
markoka101 <markoka101@gmail.com>
2024-01-10 18:28:13 -0500

    added functions to retrieve events

--- frontend/src/Components/DefaultHome.js
+++ frontend/src/Components/DefaultHome.js
@@ -1,4 +1,50 @@
+import React, { useEffect } from "react";
+
 export default function DefaultHome() {
+
+    //states for event data
+    const [data,setData] = React.useState();
+
+    //state for if page is refreshed
+    const [refresh,setRefresh] = React.useState(false);
+
+    //display events on the page loading
+    useEffect(() => {
+        const dataFetch = async() =>  {
+            const data = await (
+                await fetch('http://localhost:8080/events/all', ({
+                    method:'GET',
+                    mode:'cors',
+                    headers: {
+                        'Content-Type':'application/json'
+                    }
+                }))
+            ).json();
+            setData(data);
+        };
+
+        dataFetch();
+        setRefresh(false);
+    },[refresh]);
+
+    //make sure always working with array that isnt null/undefined
+    const eventsArr = ()=> {
+        //initialize empty array
+        const arr = [];
+
+        if(data) {
+            data.forEach(element => {
+                arr.push(element);
+            });
+        }
+
+        console.log(arr);
+
+        return arr;
+    }
+
+    eventsArr();
+
     return (
         <section id='defaultHome'>
 

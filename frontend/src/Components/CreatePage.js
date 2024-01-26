8e869f64745a9dafdbb90947655dad298b7f90cd
markoka101 <markoka101@gmail.com>
2024-01-25 19:06:29 -0500

    added create event functionality

--- frontend/src/Components/CreatePage.js
+++ frontend/src/Components/CreatePage.js
@@ -29,6 +29,7 @@ export default function CreatePage({user}) {
 
     //display events on page loading and changes
     useEffect(() =>  {
+        //created events
         const createFetch = async() => {
             const createdEvents = await (
                 await fetch('http://localhost:8080/user/createdEvents', ({
@@ -42,7 +43,7 @@ export default function CreatePage({user}) {
             ).json();
             setCreatedEvents(createdEvents);
         };
-
+        //saved events
         const savedFetch = async() => {
             const savedEvents = await (
                 await fetch('http://localhost:8080/user/savedEvents',({
@@ -66,17 +67,50 @@ export default function CreatePage({user}) {
     //event object
     const eventObj =  {
         name:eventName,
-        desc:desc,
-        date:date,
-        location:location,
-        contact:contact,
+        description:desc,
         link:link,
-        completed:false
+        location:location,
+        date:date,
+        completed:false,
+        contact:contact
+        
+
     }
 
     //handle submit from form to create event
     function createSubmit(e) {
         e.preventDefault();
+        fetch('http://localhost:8080/user/createEvent', ({
+            method:'POST',
+            mode:'cors',
+            headers: {
+                'Content-Type':'application/json',
+                'Authorization':`Bearer ${user.token}`
+            },
+            body: JSON.stringify(eventObj)
+        }))
+        .then(res => {
+            if (res.status === 201) {
+                setCreateOpen(false);
+                setRefresh(true);
+            } else {
+                alert('Something went wrong');
+            }
+        })
+        .catch(err => console.log(err));
+    }
+
+    //
+    const createdArr = (data) => {
+        const arr = [];
+
+        if(data) {
+            data.forEach(element => {
+                arr.push(element);
+            })
+        }
+
+        return arr;
     }
 
     //handle submit for the edit form
@@ -88,15 +122,125 @@ export default function CreatePage({user}) {
     function editForm() {
 
     }
+
+    //change createOpen to true when user clicks to create new event
+    function clickCreate(e) {
+        e.preventDefault();
+        setCreateOpen(!createOpen);
+    }
+
+    //change editOpen to true when user clicks to edit event
+    function clickEdit(editNum) {
+        setEditNumber(editNum);
+        setEditOpen(!editOpen);
+    }
     
     //form that appears when user wants to create an event
     function createForm()  {
+        return (
+            <div className="">
+            <form id='create-form' onSubmit={createSubmit} className="w-full lg:w-5/6 md:w-5/6 sm:w-5/6 h-3/4 flex flex-col text-lg font-bold mt-4">
+                <h1>
+                    Event Name
+                </h1>
+                <input type="text" placeholder="name" className="mb-2 pl-1"
+                value={eventName}
+                onChange={e => setEventName(e.target.value)}/>
 
+                <h1>
+                    Description
+                </h1>
+                <input type="text" placeholder="description" className="mb-2 pl-1"
+                value={desc}
+                onChange={e=>setDesc(e.target.value)}/>
+
+                <h1>
+                    Location
+                </h1>
+                <input type="text" placeholder="20 W 34th St, New York, NY, 10001" className="mb-2 pl-1"
+                value={location}
+                onChange={e=>setLocation(e.target.value)}/>
+
+                <h1>
+                    Date
+                </h1>
+                <input type="datetime-local"
+                className=""
+                value={date}
+                onChange={e=>setDate(e.target.value)}/>
+
+                <h1>
+                    Contact
+                </h1>
+                <input type="text" placeholder="Contact"className="mb-2 pl-1"
+                value={contact}
+                onChange={e=>setContact(e.target.value)}/>
+
+                <h1>
+                    Link
+                </h1>
+                <input type="text" placeholder="link" className="mb-2 pl-1"
+                value={link}
+                onChange={e=>setLink(e.target.value)}/>
+
+                <button type="submit" value='create'>
+                    Submit
+                </button>
+            </form>
+            </div>
+        );
     }
+
     return(
         <section id="createPage">
             <div className="container h-ll/12 flex flex-row items-center overflow-auto scrollbar">
-                
+                <div className="flex flex-col">
+                    <button onClick={e=>clickCreate(e)}>
+                        Create
+                    </button>
+                    {createOpen === true ? createForm() : null}
+                    <div className="my-2 py-2 px-3 w-full max-h-[75vh] overflow-auto scrollbar">
+                        {createdArr(createdEvents).map(events => {
+                            return  (
+                                <article key={events.id} className="bg-slate-50 border-black border-2 my-2 py-2  px-3  w-full">
+                                    <h1 className="pb-1 font-bold text-2xl">
+                                        {events.name}
+                                    </h1>
+                                    <h2 className="font-bold text-xl">
+                                        {dayAndTime(events.date)}
+                                    </h2>
+
+                                    <p className="font-semibold text-md">
+                                        ({convertDate(formatDate(events.date))})
+                                    </p>
+
+                                    <h2 className="font-bold text-xl">
+                                        Description:
+                                    </h2>
+                                    <p className="font-semibold text-md">
+                                        {events.description}
+                                    </p>
+
+                                    <h2 className="font-bold text-xl">
+                                        Location:
+                                    </h2>
+                                    <p className="font-semibold text-md">
+                                        {events.location}
+                                    </p>
+                                
+                                    <h2 className="font-bold text-xl">
+                                        Contact:
+                                    </h2>
+                                    <p className="font-semibold text-md">
+                                        {events.contact} <br></br>
+                                        {events.link}
+                                    </p>
+
+                                </article>
+                            )
+                        })}
+                    </div>
+                </div>
             </div>
         </section>
     );
